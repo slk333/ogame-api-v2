8cb3cbe1cbb1b90efd26fc53e39035e704a2292a
aweb33 <aweb33@icloud.com>
2018-10-24 10:37:28 -0400

    auth working

--- index.js
+++ index.js
@@ -1,112 +1,94 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user")
+
 mongoose.connect("mongodb://localhost:27017/main", {
   useNewUrlParser: true
 })
 var request = require("request")
 var parseString = require('xml2js').parseString;
+app.use(require("express-session")({
+        secret:"Whatever",
+  resave:false,
+  saveUninitialized:false
+        }))
+        
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-var playerSchema = new mongoose.Schema({
-  playerId: Number,
-  name: String,
-  alliance: String,
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+// var User = mongoose.model("User", User)
+// Player.deleteMany({}, function(error) {})
 
-})
-
-var Player = mongoose.model("Player", playerSchema)
-Player.deleteMany({}, function(error) {})
-
-
-function clearAndUpdateDatabase() {
-  // The function returns the product of p1 and p2
-
-  var playersArray = [{}];
-  request('https://s153-fr.ogame.gameforge.com/api/players.xml', function(error, response, body) {
-    //  console.log(error);  
-    //  console.log(response)  
-    //  console.log(response.statusCode)
-    //  console.log(body);
-
-
-    parseString(body, function(err, result) {
-      var players = result["players"]["player"]
-      players.forEach(function(player) {
-        playersArray.push({
-          playerId: player["$"]["id"],
-          name: player["$"]["name"]
-        })
-      })
-      Player.create(playersArray, function(error) {
-        Player.find({}, function(error, player) {
-          if (error) {
-            console.log("error retrieving player")
-            console.log(error)
-          } else {
-            console.log(player)
-          }
-        })
-      })
-    });
-  });
-}
-
-
-
-
-/*
-var panthere = new Player({
-  name:"Marshall",
-  alliance:"FURY",
-  rank:"1"
-  
-})
-*/
-
-/*
-panthere.save(function(error,player){
-if (error){
-console.log("was not able to save")
-console.log(error)
-} else{
-console.log("saved the new player into the database!")
-}
-
-})
-*/
-
-/* 
-
-var array = [{name:"Panthère"}];
-var i;
-for (i = 1; i < 10; i++) { 
-    array.push({
-      rank:(Math.floor(Math.random() * 10000))
-    });
-}
-Player.deleteMany({},function(error){})
-
-Player.create(array,function(error){
-  
-  Player.find({},function(error,player){
-if(error){
-console.log("error retrieving player")
-console.log(error)
-}
-else{
-console.log(player)
-}
-})
-  
-})
-*/
 
 
 
 
 app.get('/', function(req, res) {
-  res.send('Is this thing on?');
+  res.render("login.ejs")
 });
+
+app.get('/login', function(req, res) {
+  res.render("login.ejs")
+});
+app.post('/login', passport.authenticate("local",{
+  successRedirect: "/profile",
+  failureRedirect: "/login"
+}), function(req, res) {
+ // res.render("login.ejs")
+});
+
+
+app.get('/register', function(req, res) {
+  res.render("register.ejs")
+});
+app.post('/register', function(req, res) {
+ req.body.username
+  req.body.password
+  User.register(new User({username:req.body.username}),req.body.password,function(err,user){
+    if (err){console.log(err)
+              res.render("register.ejs")}
+    else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/profile")
+      })
+      
+    }
+  })
+});
+
+
+
+
+
+
+
+
+app.get('/profile',isLoggedIn, function(req, res) {
+  res.render("profile.ejs")
+});
+
+app.get('/logout', function(req, res) {
+  req.logout()
+   res.redirect("/login")
+});
+
+function isLoggedIn(req,res,next){
+  if (req.isAuthenticated()){
+    return next()
+  }
+  res.redirect("/login")
+  
+  
+}
 
 app.listen(3000, function() {
   console.log('Server listening on port 3000');
